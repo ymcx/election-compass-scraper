bbda9c1198853adfa633086f9c5c06d10be13ebd
Tino <89810988+ymcx@users.noreply.github.com>
2025-03-31 15:13:59 +0300

    add this sick tag

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,12 @@ async fn main() {
     let threads = misc::threads();
     let urls = misc::urls(&elections.range, &elections.url);
 
-    println!("Scraping {} with {} threads", elections.url, threads);
+    println!(
+        "{}\nURL\t[{}]\nTHREADS\t[{}]",
+        constants::TAG,
+        elections.url,
+        threads
+    );
 
     let candidates = scrape::process_urls(&urls, elections.questions, threads).await;
     misc::save(&elections.headers, &candidates, &elections.file)
